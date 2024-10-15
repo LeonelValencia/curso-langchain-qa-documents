5e820ec35468014e23ff26b98ab0b9a684703f22
Leonel Valencia <valenciaestudillo@gmail.com>
2024-10-15 10:58:08 -0600

    agregar pysqlite3 para linux

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 from functools import partial
 from operator import itemgetter
 from typing import Sequence
-
+__import__('pysqlite3')
+import sys
+sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
 import streamlit as st
 from dotenv import load_dotenv
 from langchain.base_language import BaseLanguageModel
